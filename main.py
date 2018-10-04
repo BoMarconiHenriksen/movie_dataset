755193c17636c6bd833bf0e17d3a61d5d4a2c439
Bo Henriksen <bomarconi@gmail.com>
2018-10-04 11:34:16 +0200

    Nu finder vi den mest populære danske film.

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 import downloader
 import sys
+import most_popular_danish_movie
 
 if __name__ == '__main__':
-    _, url = sys.argv
-    _, url, file_name = sys.argv
+  #  _, url = sys.argv
+ #   _, url, file_name = sys.argv
+    most_popular_danish_movie.find_most_popular_danish_movie()
 
-file_name = downloader.download(url, to_file)
+    # file_name = downloader.download(url, to_file)
