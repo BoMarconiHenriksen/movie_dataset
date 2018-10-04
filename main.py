3f3761adf299e10001301960ebda57e4f2c9045f
Bo Henriksen <bomarconi@gmail.com>
2018-10-04 16:55:35 +0200

    Nu er der en konverter fil, så vi slipper for at konverter csv til dataframe i alle klasser.

--- main.py
+++ main.py
@@ -1,9 +1,13 @@
 """ 
 The url to run this program:  https://raw.githubusercontent.com/MikkelHansen95/dataset/master/movies_metadata.csv
 """
-import downloader
 import sys
 import os
+import downloader
+import convert_csv
 
 if __name__ == '__main__':
+    global file_name
     file_name = downloader.download_file()
+
+data = convert_csv.convert_csv_to_dataframe(file_name)
