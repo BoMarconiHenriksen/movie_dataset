bb9431b7ab2a0ddbfc6837636a76ca3af1526d40
Bo Henriksen <bomarconi@gmail.com>
2018-10-05 18:45:11 +0200

    Den splitter alle ordene, men mangler at tælle dem.

---
+++ buzz_words.py
@@ -0,0 +1,28 @@
+from collections import Counter
+import pandas as pd
+
+
+def find_buzz_words(data):
+    # print(data.columns)
+    #count_words = data['overview'].str.get_dummies(sep=' ').T.dot(data['overview'])
+    # data['word_count'] = data['overview'].apply(Counter(lambda x: x.lower().split()))
+    #result = data['overview'].apply(pd.value_counts)
+    # print(pd.value_counts(data['overview'].values))
+    # print(data.info())
+    #seperated_words = data['overview'].str.get_dummies(sep=' ')
+    # print(seperated_words)
+    # print(data['overview'].str.get_dummies(sep=' ').value_counts())
+
+    # dropping null value columns to avoid errors
+    #data.dropna(inplace = True)
+    data['split_words'] = data['overview'].str.split(' ')
+    # print(type(data))
+    # print(data.columns)
+    
+    #counts = data['split_words'].value_counts()#.to_dict()
+    #print(counts)
+    # print(data['split_words'])
+    #count = data['split_words'].value_counts()
+
+    #data['count_words'] = data['split_words'].str(Counter())
+    # print(data['count_words'])
--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 """ 
-The url to run this program:  https://raw.githubusercontent.com/MikkelHansen95/dataset/master/movies_metadata.csv
+The url to run this program use this url:  https://raw.githubusercontent.com/MikkelHansen95/dataset/master/movies_metadata.csv
 """
 import sys
 import os
@@ -9,6 +9,7 @@ import most_popular_danish_movie
 import english_action_movie
 import plot_reliase_and_runtime
 import plot_adult_movies
+import buzz_words
 
 
 if __name__ == '__main__':
@@ -20,3 +21,4 @@ english_action_movie.english_action_movie_with_biggest_revenue(data)
 plot_adult_movies.plotting_adult_and_non_adult_movies(data)
 most_popular_danish_movie.find_most_popular_danish_movie(data)
 plot_reliase_and_runtime.create_plot_realise_and_runtime(data)
+buzz_words.find_buzz_words(data)
