5f1a59ea8fe743695f98dd01e884aee7b841735b
Bo Henriksen <bomarconi@gmail.com>
2018-10-05 21:58:43 +0200

    Vi plotter non-adult movies.

--- plot_adult_movies.py
+++ plot_adult_movies.py
@@ -1,10 +1,11 @@
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
+import matplotlib
 
 
 def plotting_adult_and_non_adult_movies(data):
-        
+
     # Boolean selection
     # Returns a boolean series
     filter_true_values = data['adult'] == 'True'
@@ -24,12 +25,14 @@ def plotting_adult_and_non_adult_movies(data):
     # print(false_values)
 
     # Convert strings to Date for adult movies
-    true_values.loc['release_date'] = pd.to_datetime(true_values['release_date'], errors='coerce').copy()
+    true_values.loc['release_date'] = pd.to_datetime(
+        true_values['release_date'], errors='coerce').copy()
     # print(true_values.info())
     # print(true_values['release_date'])
 
     # Convert strings to Date for non-adult movies
-    false_values.loc['release_date'] = pd.to_datetime(false_values['release_date'], errors='coerce').copy()
+    false_values.loc['release_date'] = pd.to_datetime(
+        false_values['release_date'], errors='coerce').copy()
     # print(false_values.info())
     # print(false_values['release_date'])
 
@@ -41,21 +44,15 @@ def plotting_adult_and_non_adult_movies(data):
     # print(true_values_count_dates)
 
     false_values_count_dates = false_values.groupby('release_date').size()
-    #print(false_values_count_dates)
+    # print(false_values_count_dates)
 
-    # plot_file = 'movies_per_year.png'
+    plot_file = 'movies_per_year.png'
 
-    # plt.figure()
+    false_values_count_dates.plot()
 
-    # plt.figure()
-    # true_values_count_dates.plot()
+    plt.show()
 
-    #plt.plot(true_values_count_dates, marker='o', linestyle='--', color='r', label='Adult movies')
-    #plt.plot(false_values_count_dates, marker='*', linestyle='--', color='g', label='Non-adult movies')
+    ax = false_values_count_dates.plot()
+    fig = ax.get_figure()
 
-    # plt.show()
-
-    #ax = count_dates.plot()
-    #fig = ax.get_figure()
-
-    # fig.savefig(plot_file)
+    fig.savefig(plot_file)
