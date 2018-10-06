7fc1245ebb3b03a90c6114dccb551aa837864095
Bo Henriksen <bomarconi@gmail.com>
2018-10-06 11:10:56 +0200

    Oprydning.

--- main.py
+++ main.py
@@ -5,11 +5,11 @@ import sys
 import os
 import downloader
 import convert_csv
-import most_popular_danish_movie
-import english_action_movie
-import plot_reliase_and_runtime
-import plot_adult_movies
-import buzz_words
+import lib.most_popular_danish_movie as popular_danish_movie
+import lib.english_action_movie as english_action_movie
+import lib.plot_reliase_and_runtime as reliase_and_runtime
+import lib.plot_adult_movies as plot_adult_movies
+import lib.buzz_words as buzz_words
 
 
 if __name__ == '__main__':
@@ -17,8 +17,14 @@ if __name__ == '__main__':
     file_name = downloader.download_file()
 
 data = convert_csv.convert_csv_to_dataframe(file_name)
-#english_action_movie.english_action_movie_with_biggest_revenue(data)
-#plot_adult_movies.plotting_adult_and_non_adult_movies(data)
-#most_popular_danish_movie.find_most_popular_danish_movie(data)
-#plot_reliase_and_runtime.create_plot_realise_and_runtime(data)
-buzz_words.find_buzz_words(data)
+
+print(plot_adult_movies.plotting_adult_and_non_adult_movies(data)) # With plot
+# ANIMATED MOVIES MANGLER!!!
+# MOVIE WITH HIGEST BUDGET MANGLER !!!
+
+#popular_danish_movie.find_most_popular_danish_movie(data)
+# !!! print(english_action_movie.english_action_movie_with_biggest_revenue(data))
+
+# Plots
+#reliase_and_runtime.create_plot_realise_and_runtime(data)
+#buzz_words.find_buzz_words(data)
